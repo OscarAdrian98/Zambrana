0ab06b4a37e3d543eb15b8de912d01332ba7157c
Oscar_Adrian <117079771+OscarAdrian98@users.noreply.github.com>
2024-11-04 18:48:30 +0100

    Delete importador directory

--- importador/bd/close.inc.php
+++
@@ -1,6 +0,0 @@
-<?
-if (isset($cnMySQL))
-    $cnMySQL->close();
-
-if (isset($conn))
-    sqlsrv_close($conn);
--- importador/bd/cnmy.inc.php
+++
@@ -1,5 +0,0 @@
-<?
-//$cnMySQL = new mysqli("127.0.0.1", "root", "rootzambra");
-$cnMySQL = new mysqli("", "", "");
-if (!$cnMySQL) { die($cnMySQL->error);}
-mysqli_select_db($cnMySQL, "base de datos") or die($cnMySQL->error);
